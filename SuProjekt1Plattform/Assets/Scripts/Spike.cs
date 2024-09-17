2352db487913eee6434410d9804854a3c272b16f
LinneaLD <linnealiljenrothdickman@gmail.com>
2024-09-17 13:48:28 +0200

    Fixat tile palette, lagt till spik
    också lagt till en test scene där vi kan testa det vi gjort.

---
+++ SuProjekt1Plattform/Assets/Scripts/Spike.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    [SerializeField] private int damageGiven = 1;
+    [SerializeField] private float knockForce = 200f;
+    [SerializeField] private float upwardForce = 200f;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerMovement>().TakeDamage(damageGiven);
+        }
+
+        if(other.transform.position.x > transform.position.x)
+        {
+            other.gameObject.GetComponent<PlayerMovement>().TakeKnockback(knockForce, upwardForce);
+        }
+
+        else
+        {
+            other.gameObject.GetComponent<PlayerMovement>().TakeKnockback(-knockForce, upwardForce);
+        }
+    }
+
+}
